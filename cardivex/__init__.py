8e6ce229e4a38d56188a8100c4122a4fa095edaa
Syed Umer Hannan <syedumerhannan@icloud.com>
2026-08-17 15:48:20 +0300

    Export validation utilities

--- cardivex/__init__.py
+++ cardivex/__init__.py
@@ -3,7 +3,9 @@
 from .models import Scenario, ScenarioState, EvidenceTier, Confidence
 from .engine import generate_variations, interpolate_timeline
 from .realism import realism_score
+from .defense import abnormality_score, nearest_state_distance, rescue_score
 from .audit import build_audit_record
+from .validation import validate_scenario, detect_direct_leakage
 
 __all__ = [
     "Scenario",
@@ -13,5 +15,10 @@ __all__ = [
     "generate_variations",
     "interpolate_timeline",
     "realism_score",
+    "abnormality_score",
+    "nearest_state_distance",
+    "rescue_score",
     "build_audit_record",
+    "validate_scenario",
+    "detect_direct_leakage",
 ]
